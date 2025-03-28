055c9491b1e255fbf14f7830c03c0a64c9cf67a1
iannuzzig <giannuzzi@thortech-solutions.com>
2025-03-28 15:16:56 -0300

    Dependency injection for services

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ProductService.Data.Context;
+using ProductService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDatabase")));
+builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddScoped<ISubcategoriesService, SubcategoryService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
